723dd7e505f146638c5fb74ce5e23cce90cdfaf5
김종연 <bellopenerr@gmail.com>
2025-01-13 13:03:27 +0900

    chore: annotate all

--- src/main/java/level2/Main.java
+++ src/main/java/level2/Main.java
@@ -31,14 +31,17 @@ public class Main {
         // input 받기
         int cmd = nextInt(sc);
 
-        // cmd가 0이면 종료, 이외의 int면 반복하도록
+
         if(cmd == 0) {
+          // cmd가 0이면 종료
           System.out.println("프로그램을 종료합니다.");
           break;
         } else if (cmd - 1 < menuItems.size()) {
+          // List<MenuItem>의 범위 안에 존재하면 선택한 메뉴 출력
           MenuItem selectedItem = menuItems.get(cmd - 1);
           System.out.printf("선택한 메뉴 : " + format, selectedItem.getName(), selectedItem.getPrice(), selectedItem.getDescription());
         } else {
+          // 범위를 벗어나면 다시 입력 받음
           System.out.println("존재하지 않는 메뉴입니다. 다시 선택해주세요.");
         }
       } catch (InputMismatchException e) {
